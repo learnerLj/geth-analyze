38a198797aa3ee951b1fe4bc11e37ef41bf0f03a
MUUAL <88615205+MUUAL@users.noreply.github.com>
2022-03-16 20:16:01 +0800

    Update state_processor.go
    完成core/state_processor.go文章解读

--- core/state_processor.go
+++ core/state_processor.go
@@ -56,6 +56,8 @@ func NewStateProcessor(config *params.ChainConfig, bc *BlockChain, engine consen
 // Process returns the receipts and logs accumulated during the process and
 // returns the amount of gas that was used in the process. If any of the
 // transactions failed to execute due to insufficient gas it will return an error.
+//如果因为汽油费不够而导致交易无法执行，将会报错
+//block *types.Block:区块        statedb *state.StateDB：数据库状态    cfg vm.Config：以太坊虚拟机配置
 func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg vm.Config) (types.Receipts, []*types.Log, uint64, error) {
 	var (
 		receipts    types.Receipts
@@ -67,37 +69,54 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		gp          = new(GasPool).AddGas(block.GasLimit())
 	)
 	// Mutate the block and state according to any hard-fork specs
+	// 根据硬分叉的规格改变区块和状态
+	// 如果是DAO分叉，调用ApplyDAOHardFork,进行一些退款操作
 	if p.config.DAOForkSupport && p.config.DAOForkBlock != nil && p.config.DAOForkBlock.Cmp(block.Number()) == 0 {
 		misc.ApplyDAOHardFork(statedb)
 	}
+
+	//创建一个新的以太坊虚拟机Context
 	blockContext := NewEVMBlockContext(header, p.bc, nil)
+	//返回一个新的以太坊虚拟机
 	vmenv := vm.NewEVM(blockContext, vm.TxContext{}, statedb, p.config, cfg)
+
 	// Iterate over and process the individual transactions
+	// 迭代遍历区块每一个交易
 	for i, tx := range block.Transactions() {
+		//返回事务的核心消息，包括nonce、GasLimit等等诸多东西（点进AsMessage源码可以查看）
 		msg, err := tx.AsMessage(types.MakeSigner(p.config, header.Number), header.BaseFee)
 		if err != nil {
 			return nil, nil, 0, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
 		}
+		//数据库记录当前交易的哈希，和交易索引
 		statedb.Prepare(tx.Hash(), i)
+		//每一笔交易作为消息在以太坊虚拟机中执行时，均会获得一个交易回执信息(Receipt)。形同在银行转账后，可以获得关于这笔转账的交易电子回单。
+		//回执信息分为三部分：共识信息、交易信息、区块信息
 		receipt, err := applyTransaction(msg, p.config, p.bc, nil, gp, statedb, blockNumber, blockHash, tx, usedGas, vmenv)
+
 		if err != nil {
 			return nil, nil, 0, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
 		}
+
+		//receipts是一个切片
 		receipts = append(receipts, receipt)
 		allLogs = append(allLogs, receipt.Logs...)
 	}
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
+	// engine是共识引擎模块,Finalize()形成一个基本定型的区块
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions(), block.Uncles())
 
 	return receipts, allLogs, *usedGas, nil
 }
 
+//ApplyTransaction尝试将交易应用于给定的状态数据库，并使用输入参数作为其环境。它返回交易的收据，使用的Gas和错误，如果交易失败，表明块是无效的。
 func applyTransaction(msg types.Message, config *params.ChainConfig, bc ChainContext, author *common.Address, gp *GasPool, statedb *state.StateDB, blockNumber *big.Int, blockHash common.Hash, tx *types.Transaction, usedGas *uint64, evm *vm.EVM) (*types.Receipt, error) {
-	// Create a new context to be used in the EVM environment.
+	// 对当前交易做预处理，设置交易的hash、索引、区块hash，供EVM发布新的状态日志使用
 	txContext := NewEVMTxContext(msg)
 	evm.Reset(txContext, statedb)
 
 	// Apply the transaction to the current state (included in the env).
+	// 获取一些交易信息的字段，（Ctrl跟进）
 	result, err := ApplyMessage(evm, msg, gp)
 	if err != nil {
 		return nil, err
@@ -105,9 +124,13 @@ func applyTransaction(msg types.Message, config *params.ChainConfig, bc ChainCon
 
 	// Update the state with pending changes.
 	var root []byte
+
 	if config.IsByzantium(blockNumber) {
+		//参数是deleteEmptyObjects bool
+		//Finalise方法{应该是个初始化的方法，点进源码查看均为空和新建}
 		statedb.Finalise(true)
 	} else {
+		//如果不是，statedb的状态树根哈希，同时当中会更新数据库{CtrL跟进}
 		root = statedb.IntermediateRoot(config.IsEIP158(blockNumber)).Bytes()
 	}
 	*usedGas += result.UsedGas
